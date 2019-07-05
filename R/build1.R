197c053ff51f1a4819b37233c30095f877c98a43
Vince Carey <stvjc@channing.harvard.edu>
2019-07-05 12:39:10 -0400

    improve build1 dealing with exceptions

--- R/build1.R
+++ R/build1.R
@@ -7,7 +7,7 @@ prep1 = function(pkgname) {
 	ii = rownames(installed.packages())
 	to_install = setdiff(deps, ii)
 	if (length(to_install)>0) {
-	       	ans = try(BiocManager::install(to_install, ask=FALSE, update=TRUE))
+	       	ans = try(BiocManager::install(to_install, ask=FALSE, update=FALSE))
 		if (inherits(ans, "try-error")) return(ans)
 		}
 	TRUE
@@ -16,8 +16,12 @@ prep1 = function(pkgname) {
 #' prepare and build a package tarball
 #' @param srcpath character(1) path to source folder for a package
 #' @param dest character(1) destination folder
+#' @note If preparation for building triggers a try-error, the resulting 
+#' exception object is returned.  Otherwise the result of pkgbuild::build()
+#' is returned.
 #' @export
 build1 = function(srcpath, dest=".", ...) {
 	n1 = prep1(basename(srcpath))
+        if (!is(n1, "logical")) return(n1)
 	if (n1) pkgbuild::build(srcpath, dest, ...)
 }
