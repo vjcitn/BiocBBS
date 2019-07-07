5f2d337951fde65bfeaef19c9193648e98ef6fd3
Vince Carey <stvjc@channing.harvard.edu>
2019-07-07 07:42:24 -0400

    adding ubuntu endowment support

---
+++ R/endow_ubuntu.R
@@ -0,0 +1,19 @@
+#' output of sudo apt --list installed for a suitable ubuntu environment
+#' @return a path
+#' @export
+default_packtxt = function()
+ system.file("ubuntu/ubuntu_18.04_installed_apt.txt",
+   package="BiocBBSpack")
+
+#' use sudo apt-get in system() to endow and ubuntu system for BBS activities
+#' @param packtxtfun a function providing a path to output of sudo apt --list installed
+#' @note Uses sudo apt-get install.
+#' @return Value of system().
+#' @export
+endow_ubuntu = function(packtxtfun = default_packtxt, ...) {
+  allp = readLines(packtxtfun())
+  cln = gsub("/.*", "", allp[-1])  # drop first record 'listing...'
+  string = paste(cln, collapse=" ")
+  system(paste("sudo apt-get install < ", string, collapse=" "))
+}
+
