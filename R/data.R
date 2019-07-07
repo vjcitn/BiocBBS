a1820bfe32ced9e9581eecc89d43f030269680e3
Vince Carey <stvjc@channing.harvard.edu>
2019-07-07 07:06:20 -0400

    add some tests, details on R packages needed as a vector, and improved listing of ubuntu requirements from apt --list installed

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' a vector of package names needed for a fairly complete BBS run, jul 7 2019
+#' @docType data
+#' @format character vector
+#' @examples
+#' head(installed_r)
+"installed_r"
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(BiocBBSpack)
+
+test_check("BiocBBSpack")
