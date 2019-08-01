e74e5156b2fa2cd0ff64888a788c2161798f3d73
Vince Carey <stvjc@channing.harvard.edu>
2019-08-01 11:36:55 -0400

    improve recording of failure information by serializing each try-error result

--- R/update_support.R
+++ R/update_support.R
@@ -4,10 +4,20 @@
 #' @param ncores sets options(mc.cores) value
 #' @return the result of parallel::mclapply, which may include some try-error objects
 #' @note This function will call `build1_with_buildsink` to capture the build log messages in a file.  The file will have suffix `.bldlog.txt`.
+#' If the build for package i throws a try-error, that object will be saved in an
+#' object basename(paths[i])_err, serialized to basename(paths[i])_err.rda
 #' @export
 parallel_tarballs = function(paths, dest=".", ncores=6) {
 	options(mc.cores=ncores)
-	parallel::mclapply(paths, function(x) try(build1_with_buildsink(x, dest)))
+	parallel::mclapply(paths, function(x) {
+          chk = try(build1_with_buildsink(x, dest))
+          if (inherits(chk, "try-error")) {
+             errobj = paste0(basename(x), "_err")
+             assign(errobj, chk)
+             save(get(errobj), file=paste0(errobj, ".rda"))
+          }
+        chk  # for some runs it may suffice to hand back
+        })
 }
 
 #' compare destdir to srcdir and list package paths needing tarballs
