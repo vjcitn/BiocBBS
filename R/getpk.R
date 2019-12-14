57c27d9f380ef9d03a489fb1588b259311be2af5
Vince Carey <stvjc@channing.harvard.edu>
2019-12-14 13:05:25 -0500

    Update getpk.R
    use admin/manifest repo from git.bioconductor.org

--- R/getpk.R
+++ R/getpk.R
@@ -14,10 +14,27 @@ system(sprintf("git clone --depth 1 https://git.bioconductor.org/packages/%s.git
     x))
 
 #' get vector of Bioc software package names
-#' @import BiocPkgTools
+#' @param manifest_repo_dir character(1) folder where git@git.bioconductor.org:admin/manifest has been cloned
+#' @param release character(1) git checkout will be run for this tag
 #' @return character vector
 #' @export
-bioc_software_packagelist = function() {
+bioc_software_packagelist = function(manifest_repo_dir, release="master") {
+	curd = getwd()
+	setwd(manifest_repo_dir)
+	system(paste0("git checkout ", release))
+	system("git pull")
+	txt = readLines("software.txt")[-1] # drop header
+	bl = which(nchar(txt)==0)
+	txt = txt[-bl]
+	txt = gsub("Package: ", "", txt)
+	txt = gsub(" ", "", txt)
+	txt
+}
+
+bioc_software_packagelist_old = function() {
 	ddf = buildPkgDependencyDataFrame()
 	unique(ddf$Package)
 }
+
+
+
