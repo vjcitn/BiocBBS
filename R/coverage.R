c00db93dba39cb33413908e5a8678147af43c99d
Vince Carey <stvjc@channing.harvard.edu>
2019-12-25 07:11:54 -0500

    adding coverage utility

---
+++ R/coverage.R
@@ -0,0 +1,37 @@
+
+#' @export
+covr_tab = function (x, group = c("filename", "functions"), by = "line", 
+    ...) UseMethod("covr_tab")
+
+#' produce textual summary of covr analysis
+#' @import covr
+#' @param x result of `package_coverage`
+#' @param group character(1)
+#' @param by character(1)
+#' @param \dots not used
+#' @note This is because package_coverage uses message to report summaries.
+#' @export
+covr_tab.coverage = function (x, group = c("filename", "functions"), by = "line", 
+    ...) 
+{
+    if (length(x) == 0) {
+        return()
+    }
+    group <- match.arg(group)
+    type <- attr(x, "type")
+    if (is.null(type) || type == "none") {
+        type <- NULL
+    }
+    df <- tally_coverage(x, by = by)
+    if (!NROW(df)) {
+        return(invisible())
+    }
+    percents <- tapply(df$value, df[[group]], FUN = function(x) (sum(x > 
+        0)/length(x)) * 100)
+    overall_percentage <- percent_coverage(df, by = by)
+#    message(crayon::bold(paste(collapse = " ", c(attr(x, "package")$package, 
+#        to_title(type), "Coverage: "))), format_percentage(overall_percentage))
+    by_coverage <- percents[order(percents, names(percents))]
+    list(overall = overall_percentage, specific=by_coverage)
+}
+
