406a09c9e02429abf85ec835d0a204a6c7bfb7c8
Vince Carey <stvjc@channing.harvard.edu>
2019-12-25 07:16:15 -0500

    adding covr demonstration data

--- R/data.R
+++ R/data.R
@@ -11,3 +11,10 @@
 #' @examples
 #' names(demo_rcmdchk_out)
 "demo_rcmdchk_out"
+
+#' a list of covr::package_coverage results, thinned for demonstration purposes, produced dec 2019
+#' @docType data
+#' @format list
+#' @examples
+#' cov_tab_demo[1:2]
+"cov_tab_demo"
