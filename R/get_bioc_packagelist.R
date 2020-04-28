183e8c01b8f209e989d20f833da2820c183a0669
Vince Carey <stvjc@channing.harvard.edu>
2020-04-28 06:06:38 -0400

    dealing with manifest, new vignette

---
+++ R/get_bioc_packagelist.R
@@ -0,0 +1,20 @@
+#' generate a list of packages using the manifest
+#' @note you may need credentials to clone the manifest repo
+#' @param rel character(1) release identifier, defaults to `RELEASE_3_10`
+#' @return a character vector of all packages named in selected release
+#' @export
+get_bioc_packagelist = function(rel = "RELEASE_3_10") {
+ system("git clone git@git.bioconductor.org:admin/manifest")
+ owd = getwd()
+ setwd("manifest")
+ on.exit(setwd(owd))
+ system(paste("git checkout ", rel))
+ proc_software.txt = function() {
+  x = readLines("software.txt")[-1]  # first line is comment
+  nn = which(nchar(x)==0)
+  tmp = x[-nn]
+  gsub("Package: ", "", tmp)
+ }
+ proc_software.txt()
+}
+
