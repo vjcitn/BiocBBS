77eecd637e157fe0a6413cf88c0f173d1c3d524b
Vince Carey <stvjc@channing.harvard.edu>
2020-05-27 21:56:32 -0400

    Update tarit.R
    dotarmv can be run from the .libPaths entry where binaries are 'installed', to produce binary images

--- R/tarit.R
+++ R/tarit.R
@@ -2,8 +2,15 @@ getver = function(x) { read.dcf(paste0(x, "/DESCRIPTION"))[,"Version"][[1]] }
 
 tarit = function(compiled_pkgfolder) {
    ver = getver(compiled_pkgfolder)
+   targ  = paste0(compiled_pkgfolder, "_", ver, ".tar.gz")
    system(paste0("tar czf ", compiled_pkgfolder, "_", ver, ".tar.gz ", compiled_pkgfolder), intern=TRUE)
+   targ
 }
+
+dotarmv = function(x, dest="~/binmay27") {
+    pk = tarit(x)
+    system(paste("mv ", pk, " ", dest), intern=TRUE)
+   }
 
 
 #' tar cz a binary package into 'root' folder
